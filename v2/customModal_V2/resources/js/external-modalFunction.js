154e54da4b760c992d0516ed7b4cdd73e8ae3f1f
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-22 23:11:09 +0530

    edited

--- v2/customModal_V2/resources/js/external-modalFunction.js
+++ v2/customModal_V2/resources/js/external-modalFunction.js
@@ -144,7 +144,7 @@ function showCustomConfirm(msg, result) {
     const modalBodyGif = document.createElement('img');
     modalBodyGif.classList.add('d-flex', 'mx-auto');
 
-    modalBodyGif.src = 'v2/customModal_V2/resources/images/alert.gif';
+    modalBodyGif.src = 'customModal_V2/resources/images/alert.gif';
     modalBodyGif.style.width = '100px';
     modalBody.appendChild(modalBodyGif);
 
